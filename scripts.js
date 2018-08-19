9f5bcfa371e4363abce6cba3d442e77f4c1526c5
Unknown <vero@nuagesdencre.com>
2018-08-19 20:29:08 +0100

    checking if cards clicked match; if so, removing flip

--- scripts.js
+++ scripts.js
@@ -1,6 +1,30 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
 
-function flipCard(){
-    this.classList.toggle('flip');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+function flipCard() {
+  this.classList.toggle("flip");
+
+  if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+      //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    //do cards match?
+    if (firstCard.dataset.framework === secondCard.dataset.framework){
+        //if it matches
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click',flipCard);
+    }else{
+        setTimeout(()=>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+    }, 1500);}
+  }
 }
-cards.forEach(card => card.addEventListener("click",flipCard));
+cards.forEach(card => card.addEventListener("click", flipCard));
